5cdee09914306e1be0304b322cc455ca29ff8ffb
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2023-12-11 00:15:56 +0700

    Add files via upload

---
+++ C_Leaning_main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //standart input output . header
+
+int main() {
+  printf("Nuttaphon");
+  return 0;
+
+  //comment
+  /*Comment
+  Comment
+  commnet*/
+  
+}
---
+++ C_Leaning_main/type_output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    /*
+
+    %d จำนวนเต็ม
+    %f ทศนิยม
+    %c ตัวอักษร
+    %c
+    
+    */
+    printf("Hello world");
+    return 0;
+}
